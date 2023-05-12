d1f829862972e2a09bf420821afa578d76299ce5
aruelu <arugacchi@gmail.com>
2023-05-12 13:26:11 +0900

    flatpakの全てのソフトの更新を追加

---
+++ appinstall/flatpak_allupdate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GETDISPLAY=$1
+
+LOGFILE="/tmp/flatpak_allupdate-"$$".log"
+
+zenity --info --title="flatpakの全てのソフトを更新中" --display=$GETDISPLAY --text="このウィンドウが自動で閉じるまで待って下さい" &
+GETPID=`echo $!`
+echo "flatpak update -y" > $LOGFILE
+echo ""$GETAPPID >> $LOGFILE
+flatpak update -y >> $LOGFILE
+kill $GETPID
+zenity --text-info --title=結果 --width=600  --height=400 --display=$GETDISPLAY --filename=$LOGFILE
