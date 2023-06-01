0a1d3cc43101c0e00e481e480fdeb4716821d437
aruelu <arugacchi@gmail.com>
2023-06-01 16:47:05 +0900

    「flatpakの全てのソフトのアップデート」のログファイルの表示方法変更

--- appinstall/flatpak_allupdate.sh
+++ appinstall/flatpak_allupdate.sh
@@ -4,10 +4,14 @@ GETDISPLAY=$1
 
 LOGFILE="/tmp/flatpak_allupdate-"$$".log"
 
+(
 zenity --info --title="flatpakの全てのソフトを更新中" --display=$GETDISPLAY --text="このウィンドウが自動で閉じるまで待って下さい" &
 GETPID=`echo $!`
-echo "flatpak update -y" > $LOGFILE
-echo ""$GETAPPID >> $LOGFILE
-flatpak update -y >> $LOGFILE
+echo "flatpak update -y" 
+echo ""$GETAPPID 
+flatpak update -y 
 kill $GETPID
-zenity --text-info --title="「flatpakの全てのソフト更新」の処理結果" --width=600  --height=400 --display=$GETDISPLAY --filename=$LOGFILE
+) 2>&1 | tee -a $LOGFILE | \
+        zenity --text-info --title="「flatpakの全てのソフト更新」の処理結果" \
+               --width=600  --height=400 --display=$GETDISPLAY  \
+               --auto-scroll --checkbox="処理終了を確認"
