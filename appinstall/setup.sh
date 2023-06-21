f2306304dca30ff75748c9dc38e32828d0669726
aruelu <arugacchi@gmail.com>
2023-06-21 14:10:00 +0900

    setup.shのみ複数のパッケージマネージャに対応

--- appinstall/setup.sh
+++ appinstall/setup.sh
@@ -2,11 +2,37 @@
 
 DIRBIN=$HOME/.local/myflatpak
 
-sudo apt update
-sudo apt install -y zenity
-sudo apt install -y pkexec
-sudo apt install -y flatpak
-sudo apt install -y gnome-software-plugin-flatpak
+if type apt &> /dev/null
+then
+    sudo apt update
+    package_command="apt install -y"
+
+elif type yum &> /dev/null
+then
+    package_command="yum install -y"
+
+elif type pacman &> /dev/null
+then
+    sudo pacman -Syy
+    package_command="pacman -S"
+
+elif type zypper &> /dev/null
+then
+    sudo zypper refresh
+    package_command="zypper --non-interactive in"
+
+else
+    echo "サポートされているパッケージマネージャが無いため終了します！"
+    exit 1
+
+fi
+
+echo "パッケージマネージャのコマンド：""$package_command"
+sudo $package_command zenity
+sudo $package_command pkexec
+sudo $package_command flatpak
+sudo $package_command gnome-software-plugin-flatpak
+
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 if [ ! -d $DIRBIN ]
 then
